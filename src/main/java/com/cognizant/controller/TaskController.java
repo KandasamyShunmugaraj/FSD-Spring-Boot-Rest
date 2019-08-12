609e73a2fb4966d16c84f8aabbabe42c88a1e949
Kanda <Kandasamy.Shunmugaraj@gmail.com>
2019-08-12 17:25:42 +0530

    changes for sonar fixes

--- src/main/java/com/cognizant/controller/TaskController.java
+++ src/main/java/com/cognizant/controller/TaskController.java
@@ -1,17 +1,19 @@
 package com.cognizant.controller;
 
 import java.util.List;
+
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.cognizant.entity.Task;
 import com.cognizant.service.TaskService;
 
@@ -42,7 +44,7 @@ public class TaskController {
 	public ResponseEntity<String> save(@RequestBody Task taskDto) {
         LOGGER.info("the json value of book is :::::: "+taskDto.getTaskName());
 	      taskService.addTask(taskDto);
-	      return ResponseEntity.ok().body("New Book has been saved with ID:");
+	      return ResponseEntity.ok().body("New Task has been saved");
 	   }
     
     @GetMapping("/getAllTask")
@@ -53,10 +55,10 @@ public class TaskController {
     
     
     @CrossOrigin(origins = "*" , allowedHeaders="*")
-   	@DeleteMapping("/delete/{taskId}")
-   	public ResponseEntity<String> delete(@PathVariable final String taskId) {
+   	@PutMapping("/update/{taskId}")
+   	public ResponseEntity<String> update(@PathVariable final String taskId) {
         LOGGER.info("the json value of taskId is :::::: "+taskId);
    	      taskService.updateTask(taskId);
-   	      return ResponseEntity.ok().body("New Book has been saved with ID:");
+   	      return ResponseEntity.ok().body("New Task has been update");
    	   }
 }
--- src/main/java/com/cognizant/service/TaskServiceImpl.java
+++ src/main/java/com/cognizant/service/TaskServiceImpl.java
@@ -12,90 +12,61 @@ import com.cognizant.dao.TaskDao;
 import com.cognizant.entity.Task;
 
 @Service
-public class TaskServiceImpl  implements TaskService{
-	
-	@Autowired
-	private TaskDao taskDao;
+public class TaskServiceImpl implements TaskService {
 
-	@Override
-	@Transactional
-	public void addTask(Task taskdto) {
-		Task task = new Task();
-		populateFromDTO(taskdto,task);
-		taskDao.addTask(task);
-		
-	}
+    @Autowired
+    private TaskDao taskDao;
 
-	@Override
-	@Transactional
-	public void updateTask(String taskId) {
-		Task task = taskDao.getTaskById(Long.parseLong(taskId));
-		task.setEndTask(true);
-		taskDao.updateTask(task);
-		
-	}
+    @Override
+    @Transactional
+    public void addTask(Task task) {
+        taskDao.addTask(task);
 
-	@Override
-	@Transactional
-	public Task getTaskById(Long taskId) {
-		Task task = taskDao.getTaskById(taskId);
-		Task taskDto = populateFromEntity(task);
-		if(task.getParentTask()!=null) {
-			Task parentTask = taskDao.getTaskById(Long.parseLong(task.getParentTask()));
-			taskDto.setParentTaskName(parentTask.getTaskName());
-		}
-		 return taskDto;
-	}
+    }
 
-	@Override
-	@Transactional
-	public List<Task> listTasks() {
-		List<Task> taskDtoList = new ArrayList<>();
-		List<Task> taskList = taskDao.listTasks();
-		
+    @Override
+    @Transactional
+    public void updateTask(String taskId) {
+        Task task = taskDao.getTaskById(Long.parseLong(taskId));
+        task.setEndTask(true);
+        taskDao.updateTask(task);
 
-		for (Task task : taskList) {
-		    Task taskDto = populateFromEntity(task);
-			if(task.getParentTask()!=null) {
-			Task taskReturned = taskDao.getTaskById(Long.parseLong(task.getParentTask()));
-			taskDto.setParentTaskName(taskReturned.getTaskName());
-			}
-			taskDtoList.add(taskDto);
-		}
-		
-		return taskDtoList;
-	}
+    }
 
-	
-	@Override
-	@Transactional
-	public Task getTaskByName(String taskName) {
-		return populateFromEntity(taskDao.getTaskByName(taskName));
-		
-	}
-	
-	private static void populateFromDTO(Task taskdto,Task task) {
-		
-		task.setTaskId(taskdto.getTaskId());
-		task.setTaskName(taskdto.getTaskName());
-		task.setPriority(taskdto.getPriority());
-		task.setStartDate(taskdto.getStartDate());
-		task.setEndDate(taskdto.getEndDate());
-		task.setEndTask(taskdto.isEndTask());
-		task.setParentTask(taskdto.getParentTask());
-	}
-	
-   private static Task populateFromEntity(Task task) {
-       Task taskdto = new Task();
-		
-	   taskdto.setTaskId(task.getTaskId());
-	   taskdto.setTaskName(task.getTaskName());
-	   taskdto.setPriority(task.getPriority());
-	   taskdto.setStartDate(task.getStartDate());
-	   taskdto.setEndDate(task.getEndDate());
-	   taskdto.setEndTask(task.isEndTask());
-	   taskdto.setParentTask(task.getParentTask());
-	   return taskdto;
-	}
+    @Override
+    @Transactional
+    public Task getTaskById(Long taskId) {
+        Task task = taskDao.getTaskById(taskId);
+
+        if (task.getParentTask() != null) {
+            Task parentTask = taskDao.getTaskById(Long.parseLong(task.getParentTask()));
+            task.setParentTaskName(parentTask.getTaskName());
+        }
+        return task;
+    }
+
+    @Override
+    @Transactional
+    public List<Task> listTasks() {
+        List<Task> taskDtoList = new ArrayList<>();
+        List<Task> taskList = taskDao.listTasks();
+
+        for (Task task : taskList) {
+            if (task.getParentTask() != null) {
+                Task taskReturned = taskDao.getTaskById(Long.parseLong(task.getParentTask()));
+                task.setParentTaskName(taskReturned.getTaskName());
+            }
+            taskDtoList.add(task);
+        }
+
+        return taskDtoList;
+    }
+
+    @Override
+    @Transactional
+    public Task getTaskByName(String taskName) {
+        return taskDao.getTaskByName(taskName);
+
+    }
 
 }
